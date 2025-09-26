a95c9132caf6b31213a86f4dc6202afee643b780
Jay <rright815@gmail.com>
2025-09-26 21:34:16 +0800

    302

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,29 +1,9 @@
-import { ResolveFn, Routes } from '@angular/router';
-import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { TasksService } from '../tasks/tasks.service';
-import { inject } from '@angular/core';
-
-const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
+import { Routes } from '@angular/router';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
 
 export const routes: Routes = [
   {
@@ -33,7 +13,8 @@ export const routes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    loadComponent: ()=> import('../tasks/tasks.component').then(mod => mod.TasksComponent),
+    component: TasksComponent,
+    // loadComponent: ()=> import('../tasks/tasks.component').then(mod => mod.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
@@ -42,6 +23,6 @@ export const routes: Routes = [
   {
     path: 'tasks/new',
     component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+    canDeactivate: [canLeaveEditPage],
   },
 ];
